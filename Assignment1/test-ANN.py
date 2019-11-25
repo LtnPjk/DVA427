82c83aaa07d01ced5eca060f1786000cf877674d
Felix Sjöqvist <felix.sjqv@gmail.com>
2019-11-25 17:22:41 +0100

    hehe

--- Assignment1/test-ANN.py
+++ Assignment1/test-ANN.py
@@ -5,14 +5,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
+import math
+
+smallest_error = 1000
 
 np.random.seed(0)
-feature_set, labels = datasets.make_moons(100, noise=0.10)
+#feature_set, labels = datasets.make_moons(100, noise=0.10)
 #plt.figure(figsize=(10,7))
 #plt.scatter(feature_set[:,0], feature_set[:,1], c=labels)
 #plt.scatter(feature_set[:,0], feature_set[:,1], c=labels, cmap=plt.cm.winter)
 
-labels = labels.reshape(100, 1)
+#labels = labels.reshape(100, 1)
 
 # Parse training set
 # Split lines into patients
@@ -29,21 +32,39 @@ patient_att = []
 for i, patient in enumerate(patients):
     patient_att.append(patient.split(','))
     #print(patient)
-    if i == 3:
+    #if i == 5:
         #print(patient_att)
-        break
+        #break
 
 # Take the first 18 attributes as training input
-training_inp = [attribute[0:18] for attribute in patient_att]
+training_inp = [attribute[0:19] for attribute in patient_att]
 # Take the last attribute as training output (target)
 training_oup = [[attribute[-1] for attribute in patient_att]]
-# Reverse the list
+
 
 feature_set = np.array([[float(j) for j in i] for i in training_inp])
 labels = np.array([[float(j) for j in i] for i in training_oup]).T
 
+validation_inp = feature_set[int(len(feature_set)*0.75):int(len(feature_set)*0.85)]
+test_inp = feature_set[int(len(feature_set)*0.85):int(len(feature_set))]
+feature_set = feature_set[0:int(len(feature_set)*0.75)]
+
+validation_oup = labels[int(len(labels)*0.75):int(len(labels)*0.85)]
+test_oup = labels[int(len(labels)*0.85):int(len(labels))]
+labels = labels[0:int(len(labels)*0.75)]
+
+
+print(validation_inp.shape)
+print(test_inp.shape)
 print(feature_set.shape)
-print(labels.shape)
+
+#exit()
+
+wh = np.random.rand(len(feature_set[0]), 14) 
+wo = np.random.rand(14, 1)
+lr = 0.03
+old_wh = 0
+old_wo = 0
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
@@ -51,11 +72,46 @@ def sigmoid(x):
 def sigmoid_der(x):
     return sigmoid(x) *(1-sigmoid (x))
 
-wh = np.random.rand(len(feature_set[0]), 8) 
-wo = np.random.rand(8, 1)
-lr = 0.5
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
 
-for epoch in range(100):
+def test():
+     
+    zh, ah, zo, ao, error_out = feed_forward(test_inp, test_oup)
+
+    ao = [int(round_half_up(i, 0)) for i in ao]
+
+    correct_guesses = 0
+
+    for i, j in zip(ao, test_oup):
+        if i == int(j):
+            correct_guesses = correct_guesses + 1
+    
+    print("ANN predictability: ", correct_guesses/len(test_oup))
+
+    #print(correct_guesses)
+    #print(ao)
+    #print(np.array(test_oup).T)
+
+    exit()
+
+def validation():
+    global smallest_error, old_wh, old_wo, wh, wo
+    #print("val_diff = ",smallest_error - error)
+    ii, kk, xx, yy, validation_error = feed_forward(validation_inp, validation_oup)
+    print("validation error = ",validation_error)
+    if smallest_error == 1000 or smallest_error*1.50 > validation_error:
+        smallest_error = validation_error
+        old_wh = wh
+        old_wo = wo
+    else:
+        print("validation interrupt\n")
+        wh = old_wh
+        wo = old_wo
+        test()
+
+def feed_forward(feature_set, labels):
     # feedforward
     zh = np.dot(feature_set, wh)
     ah = sigmoid(zh)
@@ -66,7 +122,18 @@ for epoch in range(100):
     # Phase1 =======================
 
     error_out = ((1 / 2) * (np.power((ao - labels), 2)))
-    print(error_out.sum())
+    #print(error_out.sum())
+
+    return zh, ah, zo, ao, error_out.sum()
+
+for epoch in range(20000):
+    zh, ah, zo, ao, error = feed_forward(feature_set, labels)
+
+    # Validation
+
+    #if epoch % 1 == 0:
+        #validation()
+
 
     dcost_dao = ao - labels
     dao_dzo = sigmoid_der(zo) 
@@ -91,3 +158,8 @@ for epoch in range(100):
 
     wh -= lr * dcost_wh
     wo -= lr * dcost_wo
+
+    #print(dcost_wh)
+    #print(dcost_wo)
+
+test()
