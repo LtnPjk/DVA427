c7d0b4756942397dec8e76d1aa23f88d6c77a478
LtnPjk <olle.olofsson97@gmail.com>
2019-12-20 14:05:12 +0100

    oOga bOogA

--- Assignment4/main.py
+++ Assignment4/main.py
@@ -41,19 +41,74 @@ def add_edge(v1, v2 , d):
     global graph
     graph[v1].append([v2, int(d)])
 
-def dijkstra():
+def dijkstra(s):
     global graph
 
+    # list of searched nodes
+    searched = []
+
+    # create dictionary for every city = [minimum distance, predecessor]
     paths = {}
     for vertex in graph.keys():
-        paths[vertex] = [[], int('Inf')]
+        paths[vertex] = [float('Inf'), None]
+
+    # set startnode distance to 0 to start there
+    paths[s][0] = 0
+    # add startnode to searched
+    searched.append(s)
+
+    # unsearched
+    unsearched = paths.copy()
+
+    # as long as we haven't searched all cities
+    while len(searched) < len(graph.keys()):
+        # get key of city with minimum distance
+        key_min = min(unsearched.keys(), key=(lambda k: unsearched[k][0]))
+        #print(paths[key_min], "key_min: ", key_min)
+        # for every neighbour of the above selected node
+        for edge in graph[key_min]:
+            # if neighbours best distance is larger than distance from current city + current citys best distance
+            if paths[edge[0]][0] > edge[1] + paths[key_min][0]:
+                 paths[edge[0]][0] = edge[1] + paths[key_min][0]
+                 paths[edge[0]][1] = key_min
+        
+        searched.append(key_min)
+        del unsearched[key_min]
+
+    #print(paths['6'][0])
+    return paths
+
+def printPath(x):
+    finalPath = []
+    a =str(x)
+    b = x
+    distance = paths[x][0]
+    while True:
+        finalPath.append(a)
+        b = a
+        a = paths[a][1]
+        if a == None:
+            break
+    finalPath.reverse()
+    print("Length:\t", distance,"\tPath from\t", b, " to ", endVertex , '\t', finalPath)    
     
-    for vertex in graph.keys():
-        paths[vertex] = 
-        for edge in vertex:
-            if edge[1] < paths[edge[0]][1]:
+#parse_file()
 
+endVertex = '5'
 
+graph = {
+    "1":[["2", 7], ["3", 9], ["6", 14]],
+    "2":[["1", 7], ["3", 10], ["4", 15]],
+    "3":[["2", 10], ["1", 9], ["6", 2], ["4", 11]],
+    "4":[["2", 15], ["3", 11], ["5", 6]],
+    "5":[["4", 6], ["6", 9]],
+    "6":[["1", 14], ["3", 2], ["5", 9]],
+    }
 
-parse_file()
+for city in graph.keys():
+    if city == endVertex:
+        continue
+    paths = dijkstra(city)
+    printPath(endVertex)
+
 print(graph)
